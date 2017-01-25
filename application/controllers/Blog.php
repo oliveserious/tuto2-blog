055225d7af5b294ea62aaf59792a7322fec6d1b7
loliveserious <loliveserious@gmail.com>
2017-01-25 02:21:13 +0100

    delete method nouvel_article

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -100,32 +100,6 @@ class Blog extends CI_Controller {
         redirect('blog/' . $this->article->alias . '_' . $id);
     }
 
-    public function nouvel_article() {
-        if (!$this->auth_user->is_connected) {
-            redirect('blog/index');
-        }
-        $this->load->helper('form');
-        $this->load->library('form_validation');
-        $this->load->model('article_status');
-        $this->set_blog_post_validation();
-        $data ['title'] = "Nouvel article";
-
-        if ($this->form_validation->run() == TRUE) {
-            $this->load->model('article');
-            $this->article->author_id = $this->auth_user->id;
-            $this->article->content = $this->input->post('content');
-            $this->article->status = $this->input->post('status');
-            $this->article->title = $this->input->post('title');
-            $this->article->save();
-            if ($this->article->is_found) {
-                redirect('blog/index');
-            }
-        }
-        $this->load->view('common/header', $data);
-        $this->load->view('blog/form', $data);
-        $this->load->view('common/footer', $data);
-    }
-
     protected function set_blog_post_validation() {
         $list = join(',', $this->article_status->codes);
         $this->form_validation->set_rules('title', 'Titre', 'required|max_length[64]');
